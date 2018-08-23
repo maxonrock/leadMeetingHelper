5e840b9b81ad38d41f67fe9bdfc01a61d95ff257
peklya <peklya>
2018-08-23 22:32:03 +0300

    The unused import has been removed

--- src/main/java/ru/cloudcrew/leadmeetinghelper/services/IUserService.java
+++ src/main/java/ru/cloudcrew/leadmeetinghelper/services/IUserService.java
@@ -2,8 +2,6 @@ package ru.cloudcrew.leadmeetinghelper.services;
 
 import ru.cloudcrew.leadmeetinghelper.persisted.User;
 
-import java.util.List;
-
 public interface IUserService {
 
     User getByEmail(String email);
